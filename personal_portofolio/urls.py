7667d066c695a8e4cbf8c58640dc9d40f871a3f5
Husni Zuhdi <hazunanafaru@gmail.com>
2021-12-05 21:22:25 +0700

    refactor personal_portofolio

--- personal_portofolio/urls.py
+++ personal_portofolio/urls.py
@@ -1,4 +1,4 @@
-"""personal_portfolio URL Configuration
+"""personal_portofolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
