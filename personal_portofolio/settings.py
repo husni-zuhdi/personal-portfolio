0616e28f0e5e981d141b4254a4d24dcf7a2a94cd
Husni Zuhdi <hazunanafaru@gmail.com>
2022-05-29 16:46:14 +0700

    Remove DEBUG mode and remove apache config file

--- personal_portofolio/settings.py
+++ personal_portofolio/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(config('DJANGO_SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',    # Localhost
