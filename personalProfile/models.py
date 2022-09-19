e060ee89454d9e4796c815817207f188c0ad7b04
Hazunanafaru <hazunanafaru@gmail.com>
2022-09-19 23:22:45 +0700

    feat(personalProfile): add model, url, and views for personal profile

--- personalProfile/models.py
+++ personalProfile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    fullname = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=20)
+    photo = models.FilePathField(path="/img")
+    description = models.TextField()
+    email = models.CharField(max_length=100)
+    github_link = models.CharField(max_length=200, default="https://github.com/Hazunanafaru/")
+    portfolio_link = models.CharField(max_length=200, default="http://husni-portofolio.xyz/")
--- personalProfile/views.py
+++ personalProfile/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from personalProfile.models import Profile
 
 # Create your views here.
+
+def profile(request):
+    profile = Profile.objects.all()
+    context = {
+        'profile' : profile
+    }
+    return render(request, 'profile.html', context)
